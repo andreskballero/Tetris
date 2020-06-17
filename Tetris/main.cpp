b5acc105367e4e191e86c504dc4726d5c11013e5
andreskballero <acaballerotoledo@gmail.com>
2020-06-17 12:10:23 +0200

    Initial Commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Andrés Caballero Toledo on 17/06/2020.
+//  Copyright © 2020 act. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
